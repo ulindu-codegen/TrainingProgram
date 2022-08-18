012c6b9a039f09b9f3aaccc2ca88c7a9ea7253a2
ulindu <ulindu@codegen.net>
2022-08-18 23:55:17 +0530

    Week 3 Practical

--- src/week_3/EmployeeReport.java
+++ src/week_3/EmployeeReport.java
@@ -11,58 +11,82 @@ public class EmployeeReport {
         //First Employee Object
         Employee employeeOne = new Employee();
 
-        String name = sc.nextLine();
-        employeeOne.setName(name);
+        System.out.print("Enter employee one name: ");
+        String name01 = sc.nextLine();
+        employeeOne.setName(name01);
 
-        int yearOfJoin = sc.nextInt();
-        employeeOne.setYearOfJoin(yearOfJoin);
+        System.out.print("Enter employee one project: ");
+        String project01 = sc.nextLine();
+        employeeOne.setProject(project01);
 
-        double salary = sc.nextDouble();
-        employeeOne.setSalary(salary);
+        System.out.print("Enter employee one address: ");
+        String address01 = sc.nextLine();
+        employeeOne.setAddress(address01);
 
-        String project = sc.nextLine();
-        employeeOne.setProject(project);
+        System.out.print("Enter employee one year of join: ");
+        int yearOfJoin01 = sc.nextInt();
+        employeeOne.setYearOfJoin(yearOfJoin01);
 
-        String address = sc.nextLine();
-        employeeOne.setAddress(address);
+        System.out.print("Enter employee one salary: ");
+        double salary01 = sc.nextDouble();
+        employeeOne.setSalary(salary01);
+
+        System.out.println();
 
         //Second Employee Object
         Employee employeeTwo = new Employee();
 
-        name = sc.nextLine();
-        employeeTwo.setName(name);
+        System.out.print("Enter employee two name: ");
+        String name02 = sc.next();
+        employeeTwo.setName(name02);
 
-        yearOfJoin = sc.nextInt();
-        employeeTwo.setYearOfJoin(yearOfJoin);
+        System.out.print("Enter employee two project: ");
+        String project02 = sc.next();
+        employeeTwo.setProject(project02);
 
-        salary = sc.nextDouble();
-        employeeTwo.setSalary(salary);
+        System.out.print("Enter employee two address: ");
+        String address02 = sc.next();
+        employeeTwo.setAddress(address02);
 
-        project = sc.nextLine();
-        employeeTwo.setProject(project);
+        System.out.print("Enter employee two year of join: ");
+        int yearOfJoin02 = sc.nextInt();
+        employeeTwo.setYearOfJoin(yearOfJoin02);
 
-        address = sc.nextLine();
-        employeeTwo.setAddress(address);
+        System.out.print("Enter employee two salary: ");
+        double salary02 = sc.nextDouble();
+        employeeTwo.setSalary(salary02);
+
+        System.out.println();
 
         //Third Employee Object
         Employee employeeThree = new Employee();
 
-        name = sc.nextLine();
-        employeeThree.setName(name);
+        System.out.print("Enter employee three name: ");
+        String name03 = sc.next();
+        employeeThree.setName(name03);
 
-        yearOfJoin = sc.nextInt();
-        employeeThree.setYearOfJoin(yearOfJoin);
+        System.out.print("Enter employee three project: ");
+        String project03 = sc.next();
+        employeeThree.setProject(project03);
 
-        salary = sc.nextDouble();
-        employeeThree.setSalary(salary);
+        System.out.print("Enter employee three address: ");
+        String address03 = sc.next();
+        employeeThree.setAddress(address03);
 
-        project = sc.nextLine();
-        employeeThree.setProject(project);
+        System.out.print("Enter employee three year of join: ");
+        int yearOfJoin03 = sc.nextInt();
+        employeeThree.setYearOfJoin(yearOfJoin03);
 
-        address = sc.nextLine();
-        employeeThree.setAddress(address);
+        System.out.print("Enter employee three salary: ");
+        double salary03 = sc.nextDouble();
+        employeeThree.setSalary(salary03);
 
+        System.out.println();
 
+        System.out.println("Name\t\t\tYear of joining\t\t\tProject\t\t\tSalary\t\t\tAddress");
+        System.out.println(employeeOne.getName() + "\t\t\t" + employeeOne.getYearOfJoin() + "\t\t\t\t" + employeeOne.getProject() + "\t\t\t" + employeeOne.getSalary() + "\t\t\t" + employeeOne.getAddress());
+        System.out.println(employeeTwo.getName() + "\t\t\t" + employeeTwo.getYearOfJoin() + "\t\t\t\t" + employeeTwo.getProject() + "\t\t\t" + employeeTwo.getSalary() + "\t\t\t" + employeeTwo.getAddress());
+        System.out.println(employeeThree.getName() + "\t\t\t" + employeeThree.getYearOfJoin() + "\t\t\t\t" + employeeThree.getProject() + "\t\t\t" + employeeThree.getSalary() + "\t\t\t" + employeeThree.getAddress());
 
     }
 
