1585324889c29b0fc9dcb24be5a6312dd2420699
ulindu <ulindu@codegen.net>
2022-09-04 23:10:59 +0530

    Week 4 Practical

--- src/week_4/task1/Calculator.java
+++ src/week_4/task1/Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
         this.number2 = number2;
     }
 
-    public Calculator(double number1, double number2,double number3) {
+    public Calculator(double number1, double number2, double number3) {
         this.number1 = number1;
         this.number2 = number2;
         this.number3 = number3;
